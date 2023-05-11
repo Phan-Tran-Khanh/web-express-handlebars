4a8d58ac9d03a9fe857b8da23d6024df08558f84
Duy Ba <duy.nguyen@whydah.xyz>
2023-05-11 18:00:22 +0700

    [Feat] Task 1: Inspiring Quotes

--- routes/task1Route.js
+++ routes/task1Route.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { emotions } = require("../data");
 
+router.get('/', (req, res) => {
+  let quotePath = '/task1/default.jpg'
+  res.render('task1', { emotions, quotePath });
+});
+
+router.get('/:emotion', (req, res) => {
+  let emotion = req.params.emotion;
+  let selectedEmotion = emotions.filter(item => item.title === emotion);
+  let quotePath = '/task1/default.jpg';
+  if (selectedEmotion) {
+    quotePath = selectedEmotion[0].quotePath;
+  }
+  console.log(emotion, quotePath)
+  res.render('task1', { emotions, quotePath });
+});
 
 module.exports = router;
